2870fcef5e13e3eaeabc8acc3f8a324cfcdba254
qianlibin <qianlibin@sysion.com>
2018-03-07 17:26:39 +0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom"
 import './App.css'
 
 class App extends Component{
